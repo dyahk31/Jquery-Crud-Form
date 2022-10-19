a5fa0fc08800d72d6558a71594dca26c6d6caf9a
Dyah Kencana Dewi <85002207+dyahk31@users.noreply.github.com>
2022-10-19 22:05:26 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-// // $(document).ready(function () {
-// //     $("#btn").click(function () {
-// //         alert("This is an alert message!");
-// //     });
-// // });
-
 const selectedRow = null;
 
 function onFormSubmit(e){
@@ -27,31 +21,32 @@ function getData(){
 function insertData(data){
     const table = $("#table") && $('tbody')[0];
     const newRow = table.insertRow(table.length);
-    const cell1 = newRow.insertCell(0);
-        cell1.innerHTML = data.nama;
-    const cell2 = newRow.insertCell(1);
-        cell2.innerHTML = data.nim;
-    const cell3 = newRow.insertCell(2);
-        cell3.innerHTML = `<a href="#" onClick='editData(this)'>Edit</a>
-                           <a href="#" onClick='deleteData(this)' id='del'>Delete</a>
-                          `;
+    newRow.innerHTML = `
+    <td>${""}</td>
+    <td>${data.nama}</td>
+    <td>${data.nim}</td>
+    <td>
+    <button type="button" class="btn btn-warning" onclick = 'editData(this)'>Edit</button>
+    <button type="button" class="btn btn-danger" onclick = 'deleteData(this)' id = 'del'>Delete</button>
+    </td>
+    `;
 }
 
 function resetForm(){
     $("#nama").val('');
     $("#nim").val('');
-    // selectedRow = null;
+    selectedRow = null;
 }
 
 function editData(td){
     const selectedRow = td.parentElement.parentElement;
-    $("#nama").val($(selectedRow.cells[0]).html());
-    $("#nim").val($(selectedRow.cells[1]).html()); 
+    $("#nama").val($(selectedRow.cells[1]).html());
+    $("#nim").val($(selectedRow.cells[2]).html()); 
 }
 
 function updateData(formData){
-    selectedRow.cells[0].html(formData.nama);
-    selectedRow.cells[1].html(formData.nim);
+    $(selectedRow).cells[1].html() = formData.val(nama);
+    $(selectedRow).cells[2].html() = formData.val(nim);
 }
 
 function deleteData(){
@@ -60,6 +55,6 @@ function deleteData(){
             $(this).closest('tr').remove();
             return false;
         })
+        resetForm();
     }
 }
-
